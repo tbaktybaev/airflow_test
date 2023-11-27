9afac68679fd460c7a07fd90b4e2c7fac0808838
Тынарбек Бактыбаев <tbaktybaev@MacBook-Pro.local>
2023-11-27 12:07:56 +0300

    interval added

--- dags/dag.py
+++ dags/dag.py
@@ -4,23 +4,23 @@ from airflow.utils.dates import days_ago
 from airflow.operators.dummy_operator import DummyOperator
 from custom_operators import DownloadCurrencyOperator, DownloadTransactionLogsOperator, MergeDataOperator, LoadToSQLiteOperator
 
-dag = DAG('dag', schedule_interval=timedelta(days=1), start_date=days_ago(1))
-t1 = DummyOperator(task_id='task_1', dag=dag)
-t2 = DummyOperator(task_id='task_2', dag=dag)
-t3 = DummyOperator(task_id='task_3', dag=dag)
-t4 = DummyOperator(task_id='task_4', dag=dag)
-t5 = DummyOperator(task_id='task_5', dag=dag)
-t6 = DummyOperator(task_id='task_6', dag=dag)
-t7 = DummyOperator(task_id='task_7', dag=dag)
+# dag = DAG('dag', schedule_interval=timedelta(days=1), start_date=days_ago(1))
+# t1 = DummyOperator(task_id='task_1', dag=dag)
+# t2 = DummyOperator(task_id='task_2', dag=dag)
+# t3 = DummyOperator(task_id='task_3', dag=dag)
+# t4 = DummyOperator(task_id='task_4', dag=dag)
+# t5 = DummyOperator(task_id='task_5', dag=dag)
+# t6 = DummyOperator(task_id='task_6', dag=dag)
+# t7 = DummyOperator(task_id='task_7', dag=dag)
 
-[t1, t2] >> t5
-t3 >> t6
-[t5, t6] >> t7
-t4
+# [t1, t2] >> t5
+# t3 >> t6
+# [t5, t6] >> t7
+# t4
 
 # Задаем аргументы для DAG
 default_args = {
-    'owner': 'you',
+    'owner': 'candidate_for_position',
     'start_date': datetime(2021, 1, 1),
     'retries': 1,
 }
@@ -30,6 +30,7 @@ dag = DAG(
     'get_transactions_dag',
     default_args=default_args,
     description='ETL DAG with CustomOperators',
+    # schedule_interval="0 0 * * *",
     schedule_interval=None,
 )
 
